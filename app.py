5e01ef0f82faf183619367feb3caa28d9aba34e5
oreshetnyk <neo343612@gmai.com>
2022-05-27 12:53:17 +0300

    Added NamerForm (FlaskForm) and Message after successful form filling

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_mysqldb import MySQL
 import mysql.connector
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -9,8 +12,15 @@ app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PORT'] = '3307' 
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'lab45db'
+app.config['SECRET_KEY'] = 'super secret key alabama sweet'
 
 mysql = MySQL(app)
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
@@ -22,15 +32,36 @@ def index():
 def form():
     return render_template('form.html')
 
+
 # Invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
+
 # Internal Server Error 
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+
+
+# Create Name Page
+@app.route('/name', methods=['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully!")
+
+    return render_template('name.html', name = name, form = form)
+
+
+
+
+
 
 @app.route('/login', methods = ['POST', 'GET'])
 def login():
